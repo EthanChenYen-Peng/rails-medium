1edc74e1204067f6847d9939f9e8e2cd1e6db043
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-24 12:29:36 +0800

    registraion form style

---
+++ app/frontend/Pages/Register.svelte
@@ -0,0 +1,35 @@
+<div class="container flex h-full flex-col items-center justify-center">
+  <h1 class="mb-[100px] font-serif text-3xl">Join Medium</h1>
+  <form
+    class="flex w-11/12 max-w-[400px] flex-col gap-3 rounded-sm bg-gray-200 px-7 py-6 font-serif "
+  >
+    <input
+      type="text"
+      value=""
+      name="username"
+      id=""
+      class="w-full px-2 py-1"
+      placeholder="Username"
+    />
+    <input
+      type="email"
+      value=""
+      name="email"
+      id=""
+      class="w-full px-2 py-1"
+      placeholder="eamil"
+    />
+    <input
+      type="password"
+      value=""
+      name="email"
+      id=""
+      class="w-full px-2 py-1"
+      placeholder="password"
+    />
+    <button
+      class="bg-green-200 px-2 py-2 text-black transition-colors hover:bg-green-700 hover:text-white"
+      >Sign Up</button
+    >
+  </form>
+</div>
