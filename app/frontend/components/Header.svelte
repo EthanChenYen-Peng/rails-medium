d72f93fd3563b45a1b99f0410d9e4de9fb8fa351
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-24 12:03:20 +0800

    header color scroll animation

--- app/frontend/components/Header.svelte
+++ app/frontend/components/Header.svelte
@@ -1,9 +1,24 @@
+<script>
+  let y;
+
+  $: changeColor = y > 300;
+</script>
+
 <nav
-  class="flex items-center border-b-[1px] border-b-black bg-amber-400 px-10 py-7"
+  class="sticky top-0 flex items-center border-b-[1px] border-b-black px-10 py-7  transition-colors duration-500 {changeColor
+    ? 'bg-white'
+    : 'bg-amber-400'}"
 >
   <span>Logo</span>
   <ul class="ml-auto flex items-center gap-4">
     <li>Signin</li>
-    <li class="rounded-3xl bg-black px-4 py-2 text-white">Get Started</li>
+    <li
+      class="rounded-3xl px-4 py-2  text-white transition-colors duration-500  {changeColor
+        ? 'bg-green-600'
+        : 'bg-black'}"
+    >
+      Get Started
+    </li>
   </ul>
 </nav>
+<svelte:window bind:scrollY={y} />
