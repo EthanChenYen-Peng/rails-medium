0844919c8ff3a2aecca1dcd24ca1a359c544be57
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-24 11:33:45 +0800

    home page hero section

--- app/frontend/Pages/Home.svelte
+++ app/frontend/Pages/Home.svelte
@@ -3,5 +3,18 @@
 </script>
 
 <Header />
+<div
+  class="flex min-h-[500px] flex-col items-start justify-center gap-6 bg-amber-400 px-6 md:gap-10 md:px-12"
+>
+  <h1 class="font-serif text-[70px] leading-[70px] md:text-[90px]">
+    Medium clone
+  </h1>
+  <h2 class="max-w-[400px] font-serif text-2xl">
+    Discover stories, thinking, and expertise from writers on any topic.
+  </h2>
+  <button class="rounded-3xl bg-black px-8 py-2 text-xl text-white"
+    >Start Reading</button
+  >
+</div>
 <p class="text-green-400">asdfasdf</p>
 <h1 class="text-2xl">home</h1>
