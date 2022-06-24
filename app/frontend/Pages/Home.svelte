d028bbc2f082de593255817f68c2559b5a12c735
EthanChenYen-Peng <ypcethan@gmail.com>
2022-06-24 11:12:12 +0800

    basic navbar style

--- app/frontend/Pages/Home.svelte
+++ app/frontend/Pages/Home.svelte
@@ -1,2 +1,7 @@
+
+<script>
+  import Header from '../components/Header.svelte'
+</script>
+<Header/>
 <p class='text-green-400'>asdfasdf</p>
 <h1 class="text-2xl">home</h1>
---
+++ app/frontend/components/Header.svelte
@@ -0,0 +1,7 @@
+<nav class="flex items-center px-10 py-7 bg-amber-400 border-b-black border-b-[1px]">
+  <span>Logo</span>
+  <ul class="flex items-center ml-auto gap-4">
+    <li>Signin</li>
+    <li class="px-4 py-2 text-white bg-black rounded-3xl">Get Started</li>
+  </ul>
+</nav>
